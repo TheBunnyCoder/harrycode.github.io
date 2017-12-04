4cbcedf690d15787bf087aee7faceac97f8586b7
harrycode <pacboy2004@gmail.com>
2017-12-04 15:35:20 +1000

    4025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252402524025240252

---
+++ 40252/main.js
@@ -0,0 +1,11 @@
+function attempt() {
+  answer = prompt(" ","source")
+  if (answer === "40252") {
+    alert("this paste is useless")
+    alert("throw it in de bin den stoopid")
+    alert("kk")
+    alert("tHGsjiyN")
+  } else {
+    alert("nope")
+  }
+}
