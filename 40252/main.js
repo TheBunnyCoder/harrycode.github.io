285a5650b336fc25d7c806d25af13ec25c23f9b5
harrycode <pacboy2004@gmail.com>
2017-12-08 08:55:59 +1000

    40252

--- 40252/main.js
+++ 40252/main.js
@@ -1,10 +1,11 @@
 function attempt() {
-  answer = prompt(" ","source")
+  answer = prompt(" ","audio")
   if (answer === "40252") {
-    alert("this paste is useless")
-    alert("throw it in de bin den stoopid")
-    alert("kk")
-    alert("tHGsjiyN")
+    alert("not anymore")
+  else if (answer === "points of interest" ) {
+    alert("talk to harry#7343, he has the answer")
+    alert("tell them that you have a request")
+    alert("https://discord.gg/pZXJrd2")
   } else {
     alert("nope")
   }
